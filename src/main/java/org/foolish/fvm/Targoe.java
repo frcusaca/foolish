cc9c00298e00c37f93b9ae5997b23501d5449794
Hc Busy <hc.busy@gmail.com>
2025-09-09 11:28:01 -0700

    Move EvalResult record into its own file

--- src/main/java/org/foolish/fvm/Targoe.java
+++ src/main/java/org/foolish/fvm/Targoe.java
@@ -39,10 +39,3 @@ public abstract class Targoe {
     public abstract EvalResult execute(Environment env);
 }
 
-/**
- * Container holding both the result of evaluating a {@link Targoe} and the
- * resulting environment.  The environment is provided so that copy-on-write
- * semantics can propagate updated environments to subsequent evaluations.
- */
-public record EvalResult(Resoe value, Environment env) {}
-
