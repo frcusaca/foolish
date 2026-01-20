093b4e4ae1dacec5bce18239a0dbdcbb30a6cb26
Hc Busy <hc.busy@gmail.com>
2026-01-19 23:04:27 -0800

    fix

--- prep_claude_package.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-RPD="$PWD/claude_mvn/repo"
-mvn -Dmaven.repo.local="$RPD" -U -DskipTests clean
-mvn -Dmaven.repo.local="$RPD" -U -DskipTests package
-mvn -Dmaven.repo.local="$RPD" -U test
-mvn -Dmaven.repo.local="$RPD" -U dependency:go-offline
-git add claude_mvn
-git commit -m "Vendor Maven repo for offline builds."
-./yacjbt.sh test
