87f3dd39cf4611cebaaa6b793fd9911d0af8a08a
Hc Busy <hc.busy@gmail.com>
2026-02-04 21:24:45 -0800

    pre-an tgravity stash

--- foolish-core-java/src/main/java/org/foolish/fvm/ubc/UnanchoredSeekFiroe.java
+++ foolish-core-java/src/main/java/org/foolish/fvm/ubc/UnanchoredSeekFiroe.java
@@ -34,9 +34,6 @@ public class UnanchoredSeekFiroe extends FiroeWithBraneMind implements Constanic
     private final int offset;
     private FIR value = null;
 
-    public UnanchoredSeekFiroe(AST.Identifier identifier) {
-        this()
-    }
     public UnanchoredSeekFiroe(AST.UnanchoredSeekExpr seekExpr) {
         super(seekExpr);
         this.offset = seekExpr.offset();
