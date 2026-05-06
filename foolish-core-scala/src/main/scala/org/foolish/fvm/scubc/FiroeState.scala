7be2da58ec736b240240ff34964040dbae6d62c1
hc busy <hcbusy+sgpu+claude@gmail.com>
2026-05-06 01:59:44 -0700

    Cleanup

--- foolish-core-scala/src/main/scala/org/foolish/fvm/scubc/FiroeState.scala
+++
@@ -1,9 +0,0 @@
-package org.foolish.fvm.scubc
-
-sealed trait FiroeState
-
-object FiroeState {
-  case class Unknown() extends FiroeState
-  case class Value(fir: FIR) extends FiroeState
-  case class Constanic() extends FiroeState
-}
