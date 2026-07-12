12989ffbf76d99ce939bf59b9f025218aa34769f
Atlas <opencode@mimo.ai>
2026-07-11 22:21:06 -0700

    Manual approval.

--- check_foolish_ubca.sh
+++ check_foolish_ubca.sh
@@ -3,7 +3,11 @@
 MINUS_F=""
 if [ "$1" == '-f' ]; then
 		  rm foolish-ubca/snapshot_tests/approved/*.new
-		  cargo clean
+		  #cargo clean
+		  # Run this loop in your terminal to target every local package
+		  for pkg in $(cargo metadata --format-version 1 | jq -r '.workspace_members[] | split(" ")[0]'); do
+				cargo clean -p $pkg
+		  done
         MINUS_F="-f"
 fi
 cargo build
